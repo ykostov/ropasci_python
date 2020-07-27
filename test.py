b7226069a9fdf6f1f32da15c2384c62dc1a9a4d5
Yasen <qskatak04@gmail.com>
2020-07-27 23:56:44 +0300

    Final commit

--- test.py
+++ test.py
@@ -1,39 +1,65 @@
-# Warining! This code is not finished yet!
-# Using: Python 3
-# Written by Yasen Kostov
-
+from random import randint
 import time
-import random
-numberlist = ['1', '2', '3']
+gamelist = ["rock", "paper", "scissors"]
+machine = gamelist[randint(0,2)]
+player = False
 
-print ("Welcome to Rock-Paper-Scissors game!")
+print ("Welcome to the game 'Rock-Paper-Scissors' written with Python3 by Yasen Kostov")
+time.sleep(5)
 print ()
-time.sleep(2)
-
-a = int(input("Choose your item (1 for rock, 2 for paper, or 3 for scissors): "))
-b = random.choice(numberlist)
+print ("The Machine is ready, are you?")
+time.sleep(4)
 print ()
 
+while player == False:
+    player = input("Chose something between 'rock', 'paper' or 'scissors': ")
+    print ()
+    time.sleep(1)
+    print ("3")
+    time.sleep(1)
+    print ("2")
+    time.sleep(1)
+    print ("1")
+    time.sleep(1)
+    print ()
+    if player == machine:
+        print ("Draw! The Machine chose ", machine, "too")
+    elif player == "rock":
+        if machine == "paper":
+            print ("You lost! The Machine chose 'paper'")
+        else:
+            print ("You Won! The Machine chose 'scissors'")
+    elif player == "paper":
+        if machine == "scissors":
+            print ("You Lost! The Machine chose 'scissors'")
+        else:
+            print ("You Won! The Machine chose 'rock'")
+    elif player == "scissors":
+        if machine == "rock":
+            print ("You Lost! The Machine chose 'rock'")
+        else:
+            print ("You Won! The Machine chose 'paper'")
+    else:
+        print ("Something went wrong.. Be sure to answer me only with 'rock', 'paper' or 'scissors'")
+    print ()
+    time.sleep(5)
+    again = input("You wanna try again? (yes, no): ")
+    if again == "yes":
+        print ()
+        player = False
+        machine = gamelist[randint(0,2)]
+    elif again == "no":
+        print ()
+        print ("Ok! Goodbye then!")
+        time.sleep(2)
+    else:
+        print ()
+        time.sleep(1)
+        print ("Uh..")
+        time.sleep(1)
+        print ("Are you absolutely sure you told me what I need to?")
+        time.sleep(3)
+        print ()
+        print ("Never mind.. Goodbye!")
 
-if a == 1:
-    if b == 1:
-        print("Draw! The Machine chose rock too")
-    if b == 2:
-        print("You Lost! The Machine chose paper.")
-    if b == 3:
-        print("You Won! The Machine chose scissors.")
-
-if a == 2:
-    if b == 1:
-        print ("You won! The Machine chose rock")
-    if b == 2:
-        print ("Draw! The Machine chose paper too")
-    if b == 3:
-        print ("You Lost! The Machine chose scissors")
-if a == 3:
-    if b == 1:
-        print ("You Lost! The Machine chose rock")
-    if b == 2:
-        print ("You win! The Machine chose paper")
-    if b == 3:
-        print ("Draw! The Machine chose scissors too")
+# Written by Yasen Kostov
